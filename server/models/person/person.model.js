ff74a887fe4bdb1b6a5844adfe407939fcfc19fb
James Sherry <james.m.sherry@googlemail.com>
2020-07-08 01:24:47 +0100

    changed casing on keys

--- server/models/person/person.model.js
+++ server/models/person/person.model.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PersonSchema = new Schema({
-  firstname: {
+  firstName: {
     type: String,
     required: true,
   },
-  lastname: {
+  lastName: {
     type: String,
     required: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
   },
 });
 
