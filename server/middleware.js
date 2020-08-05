14fdc0022957aa35ccf4d4858f01afbcf561ee93
James Sherry <james.m.sherry@googlemail.com>
2020-08-05 02:10:54 +0100

    added compression

--- server/middleware.js
+++ server/middleware.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
-const cors = require("cors");
+const compression = require("compression");
 
 module.exports = function (app) {
   // In dev mode, react-server serves the files BUT in production we BUILD the react project and express serves it out of the build folder
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/", "build")));
+    app.use(compression());
   }
 
   // parse application/x-www-form-urlencoded
