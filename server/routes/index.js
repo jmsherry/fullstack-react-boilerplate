dd7a3d0a6e0234e2d80984d991d98290babac87a
James Sherry <james.m.sherry@googlemail.com>
2020-11-12 07:11:26 +0000

    404 route handling added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,16 @@
+const path = require("path");
+
 module.exports = function (app) {
-  const API_ENDPOINT = '/api'
-  const API_VERSION = 'v1'
+  const API_ENDPOINT = "/api";
+  const API_VERSION = "v1";
   app.use(`${API_ENDPOINT}/${API_VERSION}/todos`, require("./todos.routes"));
   app.use(`${API_ENDPOINT}/${API_VERSION}/people`, require("./people.routes"));
+  app.get("*", (req, res) => {
+    if (process.env.NODE_ENV === "production") {
+      res.sendFile(path.join(__dirname, "../client/", "build/index.html"));
+    }
+  });
+  app.all("*", (req, res) => {
+    res.sendStatus(404);
+  });
 };
-
-// api/v1/people
