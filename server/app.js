82e3258ef9a9712882431d83e130371fd432bd04
James Sherry <james.m.sherry@googlemail.com>
2020-11-19 20:25:42 +0000

    small change

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { } = process.env;
+const {} = process.env;
+
+console.log("test");
 
 require("./database");
 require("./middleware")(app);
 require("./routes")(app);
-
-
-
-
-
 
 module.exports = app;
